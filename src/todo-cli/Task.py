48b38f070c72d58915008e2950cb5d6b5fbcc35c
timjuten <timurjuten@gmail.com>
2026-06-01 21:01:32 +0200

    major changes to cli.py: added add, list, remove, finish

--- src/todo-cli/Task.py
+++ src/todo-cli/Task.py
@@ -1,4 +1,4 @@
-#! usr/bin/env python3
+#!usr/bin/env python3
 class Task:
     def __init__(self, name, status: bool = False) -> None:
         self.name = name
--- src/todo-cli/cli.py
+++ src/todo-cli/cli.py
@@ -1,16 +1,78 @@
+#!usr/bin/env python3
+
+import json
+from pathlib import Path
+
+import questionary
 import typer
 from Task import Task
 
 app = typer.Typer()
 
-todo_list = []
+FILE = "task.json"
+
+
+def load_tasks():
+    if not Path(FILE).exists():
+        return []
+
+    with open(FILE, "r") as f:
+        return json.load(f)
+
+
+def save_tasks(tasks):
+    with open(FILE, "w") as f:
+        json.dump(tasks, f, indent=4)
 
 
 @app.command()
-def add(name: str, status: bool = False):
+def add(status: bool = False):
+    tasks = load_tasks()
+    name = input("Write the name of your task: ")
     task = Task(name, status)
-    print(f"")
+
+    tasks.append({"name": task.name, "status": task.status})
+
+    save_tasks(tasks)
+
+    print(f'The task "{task.name}" has been added.')
+
+
+@app.command(name="list")
+def show():
+    tasks = load_tasks()
+
+    for index, t in enumerate(tasks):
+        print(f"{index + 1}: {t['name']}, status: {t['status']}")
+
+
+@app.command()
+def remove():
+
+    tasks = load_tasks()
+    choice = questionary.select(
+        "Pick a task to remove:", choices=[t["name"] for t in tasks]
+    ).ask()
+
+    tasks = [t for t in tasks if t["name"] != choice]
+    save_tasks(tasks)
+
+    print(f"{choice} was removed.")
+
+
+@app.command()
+def finish():
+    tasks = load_tasks()
+    choice = questionary.select(
+        "Pick a task to finish", choices=[t["name"] for t in tasks]
+    ).ask()
+
+    for t in tasks:
+        if t["name"] == choice:
+            t["status"] = True
+            break
+    save_tasks(tasks)
 
 
 if __name__ == "__main__":
-    pass
+    app()
