b09a413c23dff68ab433ff01e2c35fe99f0dd2af
timjuten <timurjuten@gmail.com>
2026-06-02 09:54:47 +0200

    Update app.py

--- src/todo-cli/app.py
+++ src/todo-cli/app.py
@@ -1,20 +1,18 @@
 from textual.app import App, ComposeResult
-from textual.widgets import Footer, Header
+from textual.widgets import Footer, Header, Welcome
 
 
-class StopwatchApp(App):
-    BINDINGS = [("d", "toggle_dark", "Toggle dark mode")]
+class TodoApp(App[str]):
+    TITLE = "To Do App"
 
     def compose(self) -> ComposeResult:
+        yield Welcome()
         yield Header()
-        yield Footer()
 
-    def action_toggle_dark(self) -> None:
-        self.theme = (
-            "textual-dark" if self.theme == "textual-light" else "textual-light"
-        )
+    def on_button_pressed(self) -> None:
+        self.exit()
 
 
 if __name__ == "__main__":
-    app = StopwatchApp()
+    app = TodoApp()
     app.run()
