0023caa2f4dbadbcb5c12cc845e1c5e803d8ea24
timjuten <timurjuten@gmail.com>
2026-06-03 07:34:38 +0200

    some layout work in app.py

--- src/todo-cli/app.py
+++ src/todo-cli/app.py
@@ -1,13 +1,14 @@
 from textual.app import App, ComposeResult
-from textual.widgets import Footer, Header, Welcome
+from textual.widgets import Footer, Header, Input
 
 
 class TodoApp(App[str]):
     TITLE = "To Do App"
 
     def compose(self) -> ComposeResult:
-        yield Welcome()
         yield Header()
+        yield Input(placeholder="Add your task")
+        yield Footer()
 
     def on_button_pressed(self) -> None:
         self.exit()
