ea5dec2835b55f2e1b625a67a885b80381a58814
timjuten <timurjuten@gmail.com>
2026-06-04 07:23:23 +0200

    changes to the layout of the app itself: added working input and add button

--- src/todo-cli/app.py
+++ src/todo-cli/app.py
@@ -1,17 +1,30 @@
 from textual.app import App, ComposeResult
-from textual.widgets import Footer, Header, Input
+from textual.widgets import Button, Footer, Header, Input
+
+from cli import load_tasks, save_tasks
+from Task import Task
 
 
 class TodoApp(App[str]):
     TITLE = "To Do App"
+    CSS_PATH = "todo.tcss"
 
     def compose(self) -> ComposeResult:
         yield Header()
-        yield Input(placeholder="Add your task")
+        yield Input(placeholder="Write the name of your task", id="task")
+        yield Button(label="Add", id="add_button")
         yield Footer()
 
     def on_button_pressed(self) -> None:
-        self.exit()
+        input_widget = self.query_one("#task", Input)
+        name = input_widget.value
+
+        tasks = load_tasks()
+        task = Task(name, len(tasks), False)
+
+        tasks.append({"name": task.name, "index": task.index, "status": task.status})
+
+        save_tasks(tasks)
 
 
 if __name__ == "__main__":
